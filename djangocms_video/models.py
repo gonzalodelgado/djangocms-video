5553ad5ca1be1bb1ca2f2a56dab612f0e573e28a
Gonzalo Delgado <mail@gonzalodelgado.com>
2020-07-28 11:26:43 -0300

    Fix python_2_unicode_compatible import.

--- djangocms_video/models.py
+++ djangocms_video/models.py
@@ -15,7 +15,8 @@ except ImportError:
         for information
         """
         return instance.get_media_path(filename)
-from cms.utils.compat.dj import python_2_unicode_compatible
+from six import python_2_unicode_compatible
+
 
 from . import settings
 
